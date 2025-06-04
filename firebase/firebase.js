0338c1494e9e857de46d41427ff5ad014ceb8d72
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2025-06-04 19:33:55 -0400

    message

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,15 +3,7 @@
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCxXcu1dutjviZ20TUpqQakM6Iuw9bZOus",
-//   authDomain: "leetclone-e232e.firebaseapp.com",
-//   projectId: "leetclone-e232e",
-//   storageBucket: "leetclone-e232e.firebasestorage.app",
-//   messagingSenderId: "461753938285",
-//   appId: "1:461753938285:web:182c1abbb61543bd25ea64"
-// };
+
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
